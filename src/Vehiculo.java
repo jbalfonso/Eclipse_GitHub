1a9319143befcc5327df1db8c12fc8a3d84be8c1
alumno <alumno@alumno-1dam>
2019-03-05 14:42:15 +0100

    Reseteo a la version anterior

--- src/Vehiculo.java
+++ src/Vehiculo.java
@@ -4,9 +4,4 @@ public Vehiculo( int numPuertas, int numRuedas)
 	this. numPuertas=numPuertas;
 	this. numRuedas=numRuedas;
 }
-public void showinfo()
-{
-	System. out.println( "Soy un vehiculo");
-}
-}
 }
